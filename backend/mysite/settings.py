9ffcfe7367310a42a9ffc56608ef1c59ba63f2b7
Gustavo Junior dos Santos <gustavojuniordos@hotmail.com>
2024-08-14 20:14:43 -0300

    configurado o nginx para servir arquivos estáticos
    close #10

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.parent / 'portifolio_docker/staticfiles/static'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
