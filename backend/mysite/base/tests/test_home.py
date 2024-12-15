2d04fbd5c0c6daed8a63fbf7d5f5c5fe9a95372f
gustavodsantos <gustavojuniordos@hotmail.com>
2024-12-15 16:09:42 -0300

    ajustes nos templates

--- backend/mysite/base/tests/test_home.py
+++ backend/mysite/base/tests/test_home.py
@@ -1,6 +1,7 @@
-# import pytest
-#
-# @pytest.mark.django_db
-# def test_home_status(client):
-#     resp = client.get('/')
-#     assert resp.status_code == 200
+import pytest
+
+
+@pytest.mark.django_db
+def test_home_status(client):
+    resp = client.get('/')
+    assert resp.status_code == 200
--- backend/mysite/base/tests/test_manager.py
+++ backend/mysite/base/tests/test_manager.py
@@ -1,4 +1,4 @@
-# import pytest
+import pytest
 from django.contrib.auth import get_user_model
 from django_min_custom_user.manager import MinUserManager
 
@@ -8,8 +8,8 @@ def test_user_has_customized_manager_instance():
     assert isinstance(User.objects, MinUserManager)
 
 
-# @pytest.mark.django_db
-# def test_user_creation_with_lower_case_email():
-#     User = get_user_model()
-#     user = User.objects.create_user(email='RenZo@dev,pro.br')
-#     assert user.email == 'renzo@dev,pro.br'
+@pytest.mark.django_db
+def test_user_creation_with_lower_case_email():
+    User = get_user_model()
+    user = User.objects.create_user(email='RenZo@dev,pro.br')
+    assert user.email == 'renzo@dev,pro.br'
